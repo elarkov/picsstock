c3eb37ab1a1d786d31f24c7b813aa4f54afeee97
Evgeniy Larkov <elarkov@inbox.ru>
2023-08-28 22:41:38 +0700

    Start project

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+
+import * as functions from "./modules/functions.js";
+import * as phoneinput from "./modules/phoneinput.js";
+
+document.addEventListener('DOMContentLoaded', function () { //dom is ready
+  functions.isWebp();
+  phoneinput.phoneInput();
+});
+
+
+
+
